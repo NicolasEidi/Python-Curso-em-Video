55389f4b542667c8184aee077b785f67fcdb9a31
Nicolas Eidi <nicolas27@outlook.com>
2023-02-27 23:48:13 -0300

    Exercício 3 – Somando dois números

---
+++ Python 3 - Mundo 1 - Fundamentos em Python/ex3.py
@@ -0,0 +1,11 @@
+"""     Exercício Python 3:
+    Crie um programa que leia dois números 
+    e mostre a soma entre eles.  
+
+    Resolução do exercício:
+"""
+
+a = int(input('Número 1: '))
+b = int(input('Número 2: '))
+
+print(f'A soma entre {a} e {b} resulta em {a+b}')
