8af5395735e699c3ec7c954e4b58533b1c24fa42
Nicolas Eidi <nicolas27@outlook.com>
2023-02-27 23:32:18 -0300

    Exercício 2 – Respondendo ao Usuário

---
+++ Python 3 - Mundo 1 - Fundamentos em Python/ex2.py
@@ -0,0 +1,9 @@
+""""    Exercício Python 2: 
+    Faça um programa que leia o nome de uma pessoa 
+    e mostre uma mensagem de boas-vindas.
+
+    Resolução do exercício:
+"""
+
+nome = str(input('Nome: '))
+print(f'Seja bem vindo, {nome.capitalize()}!')
