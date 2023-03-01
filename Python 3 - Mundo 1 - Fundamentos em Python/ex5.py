03b5ece8ed4f6af5a44a863c0c0ad0dd04dffe4d
Nicolas Eidi <nicolas27@outlook.com>
2023-02-28 21:25:31 -0300

    Exercício 2 – Respondendo ao Usuário

--- Python 3 - Mundo 1 - Fundamentos em Python/ex5.py
+++
@@ -1,9 +0,0 @@
-"""     Exercício Python 5: 
-    Faça um programa que leia um número Inteiro e 
-    mostre na tela o seu sucessor e seu antecessor.
-
-    Resolução do exercício:
-"""
-
-numero = int(input('Digite um número: '))
-print('Sucessor de {} é {} e seu antecessor é {}'.format(numero, (numero+1), (numero-1)))
