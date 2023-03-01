821c44e45486a526d430d2f3cfcc7556423bb8f2
Nicolas Eidi <nicolas27@outlook.com>
2023-02-28 21:42:56 -0300

    Exercício 53 – Detector de Palíndromo

---
+++ Python 3 - Mundo 2 - Estruturas de Controle/ex053.py
@@ -0,0 +1,22 @@
+"""     Exercício Python 53: 
+    Crie um programa que leia uma frase qualquer e diga 
+    se ela é um palíndromo, desconsiderando os espaços. 
+    Exemplos de palíndromos:
+    APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, 
+    O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.
+
+    Resolução do exercício:
+"""
+
+frase = str(input('Frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+
+if inverso == junto:
+    print('É um PALÍNDROMO')
+else:
+    print('Não é um PALÍNDROMO')
