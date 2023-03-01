9168840a81d794aeb7b59e309563070ec33feb42
Nicolas Eidi <nicolas27@outlook.com>
2023-02-28 21:41:57 -0300

    Exercício 46 – Contagem regressiva

---
+++ Python 3 - Mundo 2 - Estruturas de Controle/ex046.py
@@ -0,0 +1,14 @@
+"""     Exercício Python 46: 
+    Faça um programa que mostre na tela uma contagem regressiva 
+    para o estouro de fogos de artifício, indo de 10 até 0, 
+    com uma pausa de 1 segundo entre eles.
+
+    Resolução do exercício:
+"""
+
+from time import sleep
+
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print('Feliz Ano Novo!!!')
